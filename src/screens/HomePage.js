184cfaa502937d67d4c2fe81b9c75989f9e5c905
Abhijeet Prasad <abhi.prasad16@gmail.com>
2021-06-08 23:12:34 +0530

    integrate : homepage

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,20 @@
 import NavBar from '../components/Navbar'
 import Features from '../components/Features'
+import Hero from '../components/Hero'
+import Stats from '../components/Stats'
+import { Divider } from '@chakra-ui/layout';
+import Footer from '../components/Footer';
+
 function HomePage() {
     return (
         <div>
             <NavBar />
+            <Hero/>
+            <Stats/>
+            <Divider/>
             <Features />
+            <br/>
+            <Footer/>
         </div>
     );
 }
