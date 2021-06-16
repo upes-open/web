2cdd1dea0e481f6952936b1879844a578cd26e04
Raghav Agarwal <ragharwal@gmail.com>
2021-06-16 23:19:22 +0530

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './screens/HomePage'
+import AboutPage from './screens/AboutPage'
+import ContactPage from './screens/ContactPage'
 function App() {
   return (
-      <HomePage/>
+    <Router>
+      <Switch>
+        <Route exact path = "/web">
+          <HomePage/>
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path = "/about">
+          <AboutPage/>
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path = "/contact">
+          <ContactPage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
