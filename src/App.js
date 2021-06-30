3c6cf43349130807f3644508690588556391a2ff
Abhijeet Prasad <abhi.prasad16@gmail.com>
2021-06-30 13:46:10 +0530

    fix: router and heroku deploy

--- src/App.js
+++ src/App.js
@@ -8,32 +8,13 @@ import EventPage from "./screens/EventPage";
 
 function App() {
   return (
-    <Router basename="/web" >
+    <Router>
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/projects' component={Projects} />
         <Route path='/team' component={TeamPage} />
         <Route path='/about' component={AboutPage} />
         <Route path='/events' component={EventPage} />
-        {/* <Route exact path="/">
-          <HomePage />
-        </Route>
-      
-        <Route exact path="/projects" component={Projects}></Route>
-      
-        <Route exact path="/team" component={TeamPage}></Route>
-      
-        <Route exact path="/about">
-          <AboutPage />
-        </Route>
-      
-        <Route exact path="/contact">
-          <ContactPage />
-        </Route>
-     
-        <Route exact path="/event">
-          <EventPage />
-        </Route> */}
       </Switch>
     </Router>
   );
