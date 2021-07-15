179429e8b6ae853333e1d3e937f63c54827fb972
Sanyam Mehendiratta <sanyammehendiratta2000@gmail.com>
2021-07-15 12:33:21 +0530

    Events page

--- src/screens/EventPage.js
+++ src/screens/EventPage.js
@@ -1,7 +1,45 @@
-import React from "react";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import maintain from "../assets/maintain.svg";
+import { Heading } from "@chakra-ui/react";
 
-const EventPage = () => {
-  return <div> Event Page </div>;
-};
+function EventPage() {
+  return (
+    <div>
+      <NavBar />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "4rem",
+          flexWrap: "wrap",
+        }}
+      >
+        <div>
+          <Heading
+            fontSize={"5xl"}
+            py={10}
+            color={"green.400"}
+            fontWeight={"bold"}
+          >
+            This page is currently under maintenance
+          </Heading>
+        </div>
+
+        <div>
+          <img
+            width="500px"
+            height="450px"
+            src={maintain}
+            alt="under maintainece"
+          />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
 
 export default EventPage;
