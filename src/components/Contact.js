97305e88db6a6f1b26f50a887e4e5ab36435865d
Sanyam Mehendiratta <sanyammehendiratta2000@gmail.com>
2021-07-16 15:56:21 +0530

    updated email
    - Updated email in Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -51,7 +51,7 @@ export default function Contact() {
             <Feature
               icon={<Icon as={FaMailBulk} color={"yellow.500"} w={5} h={5} />}
               iconBg={useColorModeValue("yellow.100", "yellow.900")}
-              text={"upesopen@gmail.com"}
+              text={"opencommunity@ddn.upes.ac.in"}
             />
             <Feature
               icon={<Icon as={FaLinkedin} color={"green.500"} w={5} h={5} />}
