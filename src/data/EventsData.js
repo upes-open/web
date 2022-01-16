cd18bfa1cd764cc698e6ce9b6747954f072c1611
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-01-16 15:41:42 +0530

    dynamic and responsive

---
+++ src/data/EventsData.js
@@ -0,0 +1,50 @@
+export const EventsData = [
+  {
+    name: "Event name",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  aliquyam erat, sed diam voluptua. At vero eos et accusam etjusto duo dolores et ea rebum.",
+    imageURL:
+      "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg",
+    date: "xx/xx/xxxx",
+    registeration: 0,
+  },
+  {
+    name: "Event name",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  aliquyam erat, sed diam voluptua. At vero eos et accusam etjusto duo dolores et ea rebum.",
+    imageURL:
+      "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg",
+    date: "xx/xx/xxxx",
+    registeration: 0,
+  },
+  {
+    name: "Event name",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  aliquyam erat, sed diam voluptua. At vero eos et accusam etjusto duo dolores et ea rebum.",
+    imageURL:
+      "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg",
+    date: "xx/xx/xxxx",
+    registeration: 0,
+  },
+  {
+    name: "Event name",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  aliquyam erat, sed diam voluptua. At vero eos et accusam etjusto duo dolores et ea rebum.",
+    imageURL:
+      "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg",
+    date: "xx/xx/xxxx",
+    registeration: 0,
+  },
+  {
+    name: "Event name",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  aliquyam erat, sed diam voluptua. At vero eos et accusam etjusto duo dolores et ea rebum.",
+    imageURL:
+      "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg",
+    date: "xx/xx/xxxx",
+    registeration: 0,
+  },
+  {
+    name: "Event name",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna  aliquyam erat, sed diam voluptua. At vero eos et accusam etjusto duo dolores et ea rebum.",
+    imageURL:
+      "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg",
+    date: "xx/xx/xxxx",
+    registeration: 0,
+  },
+];
