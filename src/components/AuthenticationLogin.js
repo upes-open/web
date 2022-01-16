76519a07b3119a991ececa8da2b7b6cc3b4323e1
Raghav Agarwal <55791859+ragharwal@users.noreply.github.com>
2022-01-16 19:02:19 +0530

    Dev (#37)

---
+++ src/components/AuthenticationLogin.js
@@ -0,0 +1,92 @@
+// import {
+//   FormControl,
+//   FormLabel,
+//   Box,
+//   Heading,
+//   Input,
+//   Image,
+//   Button,
+//   SimpleGrid,
+//   useToast,
+// } from "@chakra-ui/react";
+// import React from "react";
+// import { useForm } from "react-hook-form";
+
+// export default function AuthenticationLogin() {
+//   const toast = useToast();
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+//   const onSubmit = (data) => {
+//     if (data.privatekey === "private key") {
+//       sessionStorage.setItem("lastname", data.privatekey);
+//       window.location.href = "/CreateEvents";
+//     } else {
+//       toast({
+//         title: "Incorrect Key!",
+//         status: "error",
+//         duration: 3000,
+//         isClosable: true,
+//       });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <SimpleGrid
+//         width="full"
+//         align="center"
+//         justifyContent="center"
+//         columns={2}
+//         minChildWidth="350px"
+//         py={10}
+//         my={5}
+//       >
+//         <Box p={2} my={4}>
+//           <Image
+//             h={"342"}
+//             w={"500"}
+//             src="https://raw.githubusercontent.com/upes-open/web/dev/src/assets/authentication.png"
+//             alt=""
+//           />
+//         </Box>
+//         <Box
+//           maxWidth="500px"
+//           p={8}
+//           py={12}
+//           my={4}
+//           borderWidth={1}
+//           borderRadius={8}
+//           boxShadow="lg"
+//         >
+//           <Box textAlign="center">
+//             <Heading>Login</Heading>
+//           </Box>
+//           <Box my={4} textAlign="left">
+//             <form onSubmit={handleSubmit(onSubmit)}>
+//               <FormControl mt={6}>
+//                 <FormLabel py={2}>Private Key</FormLabel>
+//                 <Input
+//                   type="password"
+//                   placeholder="**********"
+//                   {...register("privatekey")}
+//                 />
+//               </FormControl>
+//               <Button
+//                 width="full"
+//                 mt={4}
+//                 type="submit"
+//                 colorScheme={"green"}
+//                 bg={"green.400"}
+//               >
+//                 Authenticate
+//               </Button>
+//             </form>
+//           </Box>
+//         </Box>
+//       </SimpleGrid>
+//     </div>
+//   );
+// }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,4 @@
-/* import Image from "../assets/maintain.svg"; */
 import * as React from "react";
-/* import icon */
-
 import {
   Box,
   Button,
@@ -12,395 +9,137 @@ import {
   Container,
   SimpleGrid,
 } from "@chakra-ui/react";
+import { EventsData } from "../data/EventsData";
+
+function isRegistrationOpen(registerationFlag) {
+  if (registerationFlag) {
+    return (
+      <Button
+        mt={4}
+        fontSize={"sm"}
+        rounded={"full"}
+        bg={"green.400"}
+        onClick={() => {
+          window.location.href = registerationFlag;
+        }}
+        color={"white"}
+        boxShadow={
+          "0px 1px 25px -5px rgb(80 200 120 / 48%), 0 10px 10px -5px rgb(80 200 120 / 43%)"
+        }
+        _hover={{
+          bg: "green.500",
+        }}
+        _focus={{
+          bg: "green.500",
+        }}
+      >
+        Register
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        mt={4}
+        fontSize={"sm"}
+        rounded={"full"}
+        bg={"green.400"}
+        color={"white"}
+        boxShadow={
+          "0px 1px 25px -5px rgb(80 200 120 / 48%), 0 10px 10px -5px rgb(80 200 120 / 43%)"
+        }
+        _hover={{
+          bg: "green.500",
+        }}
+        _focus={{
+          bg: "green.500",
+        }}
+      >
+        Registeration Closed
+      </Button>
+    );
+  }
+}
 
 export default function Cards() {
   return (
     /* box of color green */
     <>
-      <Container maxW="80vw" centerContent>
-    <SimpleGrid columns={2} spacing={10} >  
+      <Container maxW="80vw" centerContent py={8}>
+        <SimpleGrid columns={[1, null, 2]} spacing={20}>
+          {EventsData.map((data) => (
+            <Box
+              maxW="sm"
+              w={"full"}
+              boxShadow={"2xl"}
+              rounded={"md"}
+              p="50px"
+              backgroundColor={"white.100"}
+              overflow={"hidden"}
+            >
+              <Box h={"30vh"} mb={6}>
+                <Image
+                  src={data.imageURL}
+                  h={"full"}
+                  w={"full"}
+                  layout={"fill"}
+                  overflow={"hidden"}
+                />
+              </Box>
+              <Stack>
+                {/* add upper margin to heading */}
+                <Heading
+                  fontWeight={600}
+                  fontSize={"4xl"}
+                  fontFamily={"body"}
+                  color={"green"}
+                  marginTop={1}
+                  textAlign={"center"}
+                >
+                  {data.name}
+                </Heading>
+                <Text color={"gray.500"} textAlign={"center"}>
+                  {data.text}
+                </Text>
+                {/* events date */}
+                <Text color={"gray.500"} fontWeight={600} textAlign={"center"}>
+                  Date: {data.date}
+                </Text>
+                {/* adding socialButton twitter, instagram, linkedin */}
+                {/*
+                <Box d={"flex"} justifyContent={"center"}>
+                  <a href="https://github.com/Joeyryanbridges">
+                    <Image
+                      src={
+                        "https://img.icons8.com/cute-clipart/64/000000/twitter.png"
+                      }
+                      size={"10px"}
+                      p={2}
+                    />
+                  </a>
 
-    <Box
-        maxW={"30vw"}
-    
-        w={"full"}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        p={6}
-        m={10}
-        backgroundColor={"white.100"}
-        overflow={"hidden"}
-      >
-        <Box h={"30vh"} mb={6}>
-          <Image
-            src={
-              "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg"
-            }
-            h={"full"}
-            w={"full"}
-            layout={"fill"}
-            overflow={"hidden"}
-          />
-        </Box>
-        <Stack>
-          {/* add upper margin to heading */}
-          <Heading
-            fontWeight={600}
-            fontSize={"4xl"}
-            fontFamily={"body"}
-            color={"green"}
-            marginTop={1}
-            textAlign={"center"}
-          >
-            Event name
-          </Heading>
-
-          <Text color={"gray.500"} textAlign={"center"}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-          {/* events date */}
-          <Text color={"gray.500"} fontWeight={600} textAlign={"center"}>
-            Date: xx/xx/xxxx
-          </Text>
-
-          {/* adding socialButton twitter, instagram, linkedin */}
-
-          <Box d={"flex"} justifyContent={"center"}>
-            <a href="https://github.com/Joeyryanbridges">
-              <Image
-                src={
-                  "https://img.icons8.com/cute-clipart/64/000000/twitter.png"
-                }
-                size={"10px"}
-                p={2}
-              />
-            </a>
-
-            <Image
-              src={"https://img.icons8.com/cute-clipart/64/000000/linkedin.png"}
-              size={"10px"}
-              p={2}
-            />
-            <Image
-              src={
-                "https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"
-              }
-              size={"10px"}
-              p={2}
-            />
-          </Box>
-
-          {/* register button */}
-          <Button
-            mt={4}
-            fontSize={"sm"}
-            rounded={"full"}
-            bg={"green.400"}
-            color={"white"}
-            boxShadow={
-              "0px 1px 25px -5px rgb(80 200 120 / 48%), 0 10px 10px -5px rgb(80 200 120 / 43%)"
-            }
-            _hover={{
-              bg: "green.500",
-            }}
-            _focus={{
-              bg: "green.500",
-            }}
-          >
-            Register
-          </Button>
-        </Stack>
-      </Box> <Box
-        maxW={"30vw"}
-    
-        w={"full"}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        p={6}
-        m={10}
-        backgroundColor={"white.100"}
-        overflow={"hidden"}
-      >
-        <Box h={"30vh"} mb={6}>
-          <Image
-            src={
-              "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg"
-            }
-            h={"full"}
-            w={"full"}
-            layout={"fill"}
-            overflow={"hidden"}
-          />
-        </Box>
-        <Stack>
-          {/* add upper margin to heading */}
-          <Heading
-            fontWeight={600}
-            fontSize={"4xl"}
-            fontFamily={"body"}
-            color={"green"}
-            marginTop={1}
-            textAlign={"center"}
-          >
-            Event name
-          </Heading>
-
-          <Text color={"gray.500"} textAlign={"center"}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-          {/* events date */}
-          <Text color={"gray.500"} fontWeight={600} textAlign={"center"}>
-            Date: xx/xx/xxxx
-          </Text>
-
-          {/* adding socialButton twitter, instagram, linkedin */}
-
-          <Box d={"flex"} justifyContent={"center"}>
-            <a href="https://github.com/Joeyryanbridges">
-              <Image
-                src={
-                  "https://img.icons8.com/cute-clipart/64/000000/twitter.png"
-                }
-                size={"10px"}
-                p={2}
-              />
-            </a>
-
-            <Image
-              src={"https://img.icons8.com/cute-clipart/64/000000/linkedin.png"}
-              size={"10px"}
-              p={2}
-            />
-            <Image
-              src={
-                "https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"
-              }
-              size={"10px"}
-              p={2}
-            />
-          </Box>
-
-          {/* register button */}
-          <Button
-            mt={4}
-            fontSize={"sm"}
-            rounded={"full"}
-            bg={"green.400"}
-            color={"white"}
-            boxShadow={
-              "0px 1px 25px -5px rgb(80 200 120 / 48%), 0 10px 10px -5px rgb(80 200 120 / 43%)"
-            }
-            _hover={{
-              bg: "green.500",
-            }}
-            _focus={{
-              bg: "green.500",
-            }}
-          >
-            Register
-          </Button>
-        </Stack>
-      </Box>
-      {/* 3rd */}
-      <Box
-        maxW={"30vw"}
-    
-        w={"full"}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        p={6}
-        m={10}
-        backgroundColor={"white.100"}
-        overflow={"hidden"}
-      >
-        <Box h={"30vh"} mb={6}>
-          <Image
-            src={
-              "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg"
-            }
-            h={"full"}
-            w={"full"}
-            layout={"fill"}
-            overflow={"hidden"}
-          />
-        </Box>
-        <Stack>
-          {/* add upper margin to heading */}
-          <Heading
-            fontWeight={600}
-            fontSize={"4xl"}
-            fontFamily={"body"}
-            color={"green"}
-            marginTop={1}
-            textAlign={"center"}
-          >
-            Event name
-          </Heading>
-
-          <Text color={"gray.500"} textAlign={"center"}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-          {/* events date */}
-          <Text color={"gray.500"} fontWeight={600} textAlign={"center"}>
-            Date: xx/xx/xxxx
-          </Text>
-
-          {/* adding socialButton twitter, instagram, linkedin */}
-
-          <Box d={"flex"} justifyContent={"center"}>
-            <a href="https://github.com/Joeyryanbridges">
-              <Image
-                src={
-                  "https://img.icons8.com/cute-clipart/64/000000/twitter.png"
-                }
-                size={"10px"}
-                p={2}
-              />
-            </a>
-
-            <Image
-              src={"https://img.icons8.com/cute-clipart/64/000000/linkedin.png"}
-              size={"10px"}
-              p={2}
-            />
-            <Image
-              src={
-                "https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"
-              }
-              size={"10px"}
-              p={2}
-            />
-          </Box>
-
-          {/* register button */}
-          <Button
-            mt={4}
-            fontSize={"sm"}
-            rounded={"full"}
-            bg={"green.400"}
-            color={"white"}
-            boxShadow={
-              "0px 1px 25px -5px rgb(80 200 120 / 48%), 0 10px 10px -5px rgb(80 200 120 / 43%)"
-            }
-            _hover={{
-              bg: "green.500",
-            }}
-            _focus={{
-              bg: "green.500",
-            }}
-          >
-            Register
-          </Button>
-        </Stack>
-      </Box>
-      {/* 4th */}
-      <Box
-        maxW={"30vw"}
-    
-        w={"full"}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        p={6}
-        m={10}
-        backgroundColor={"white.100"}
-        overflow={"hidden"}
-      >
-        <Box h={"30vh"} mb={6}>
-          <Image
-            src={
-              "https://images.cloudflareapps.com/ij5s5higSzWcOB6vks5Q_background-1.jpeg"
-            }
-            h={"full"}
-            w={"full"}
-            layout={"fill"}
-            overflow={"hidden"}
-          />
-        </Box>
-        <Stack>
-          {/* add upper margin to heading */}
-          <Heading
-            fontWeight={600}
-            fontSize={"4xl"}
-            fontFamily={"body"}
-            color={"green"}
-            marginTop={1}
-            textAlign={"center"}
-          >
-            Event name
-          </Heading>
-
-          <Text color={"gray.500"} textAlign={"center"}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-          {/* events date */}
-          <Text color={"gray.500"} fontWeight={600} textAlign={"center"}>
-            Date: xx/xx/xxxx
-          </Text>
-
-          {/* adding socialButton twitter, instagram, linkedin */}
-
-          <Box d={"flex"} justifyContent={"center"}>
-            <a href="https://github.com/Joeyryanbridges">
-              <Image
-                src={
-                  "https://img.icons8.com/cute-clipart/64/000000/twitter.png"
-                }
-                size={"10px"}
-                p={2}
-              />
-            </a>
-
-            <Image
-              src={"https://img.icons8.com/cute-clipart/64/000000/linkedin.png"}
-              size={"10px"}
-              p={2}
-            />
-            <Image
-              src={
-                "https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"
-              }
-              size={"10px"}
-              p={2}
-            />
-          </Box>
-
-          {/* register button */}
-          <Button
-            mt={4}
-            fontSize={"sm"}
-            rounded={"full"}
-            bg={"green.400"}
-            color={"white"}
-            boxShadow={
-              "0px 1px 25px -5px rgb(80 200 120 / 48%), 0 10px 10px -5px rgb(80 200 120 / 43%)"
-            }
-            _hover={{
-              bg: "green.500",
-            }}
-            _focus={{
-              bg: "green.500",
-            }}
-          >
-            Register
-          </Button>
-        </Stack>
-      </Box>
-      
-  
-    </SimpleGrid>
-    </Container>
+                  <Image
+                    src={
+                      "https://img.icons8.com/cute-clipart/64/000000/linkedin.png"
+                    }
+                    size={"10px"}
+                    p={2}
+                  />
+                  <Image
+                    src={
+                      "https://img.icons8.com/cute-clipart/64/000000/instagram-new.png"
+                    }
+                    size={"10px"}
+                    p={2}
+                  />
+                </Box>
+                */}
+                {/* register button */}
+                {isRegistrationOpen(data.registeration)}
+              </Stack>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Container>
     </>
   );
 }
