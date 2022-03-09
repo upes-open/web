2701fd72b5b20bbedc84dba8b9cfff73ed7040bd
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-03-09 20:02:39 +0530

    created RegisterPage.js

--- src/App.js
+++ src/App.js
@@ -5,38 +5,24 @@ import TeamPage from "./screens/TeamPage";
 import AboutPage from "./screens/AboutPage";
 import ContactPage from "./screens/ContactPage";
 import EventPage from "./screens/EventPage";
-
-
-import AuthenticationPage from "./screens/AuthenticationPage";
-import CreateEvents from "./screens/CreateEvent";
+import RegisterPage from "./screens/RegisterPage";
 import Page404 from "./screens/Page404";
-
 
 function App() {
   return (
     <Router>
       <Switch>
-
-        <Route path='/' exact component={HomePage} />
-        <Route path='/projects' component={Projects} />
-        <Route path='/team' component={TeamPage} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/contact' component={ContactPage} />
-        <Route path='/events' component={EventPage} />
-
         <Route path="/" exact component={HomePage} />
         <Route path="/projects" component={Projects} />
         <Route path="/team" component={TeamPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/contact" component={ContactPage} />
         <Route path="/events" component={EventPage} />
-        <Route path="/authenticate" component={AuthenticationPage} />
-        <Route path="/CreateEvents" component={CreateEvents} />
+        <Route path="/RegisterPage" component={RegisterPage} />
         <Route path="*" component={Page404} />
-
       </Switch>
     </Router>
   );
-} 
+}
 
 export default App;
