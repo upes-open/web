90a3d45de505471d89d0ee5876c906ba9258c3d2
anuva <garganuva@gmail.com>
2022-03-22 22:35:47 +0530

    corrected data

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -15,7 +15,7 @@ export const TeamData = [
     },
     {
         name: "Aradhya Singh",
-        designation: "Treasurer, Vice-President (2021-22)",
+        designation: "Treasurer",
         imageURL: "https://media-exp1.licdn.com/dms/image/C5603AQE64DOjXVHXsg/profile-displayphoto-shrink_800_800/0/1626534870614?e=1653523200&v=beta&t=TFaENfQVHW2OZQootoOiXMFRLIWM1ynLvdvGphUYO70",
         LinkedIn: "https://www.linkedin.com/in/aradhya-singh-7993751a8/",
         GitHub: "https://github.com/optimizer-aa",
@@ -65,15 +65,15 @@ export const TeamData = [
     {
         name: "Sarthak Tiwari",
         designation: "Design & Website Lead",
-        imageURL: "https://media-exp1.licdn.com/dms/image/C4D03AQG29Al4a5ZJgw/profile-displayphoto-shrink_800_800/0/1617885196967?e=1653523200&v=beta&t=Zp0XqBJwB8IJyYrp2GsoSbNasE1pNwvxFUyg2JPdB3I",
-        LinkedIn: "https://www.linkedin.com/in/sarthak-tiwari-692227176/",
+        imageURL: "https://media-exp1.licdn.com/dms/image/C4D03AQGaQtq0Ej-U6A/profile-displayphoto-shrink_800_800/0/1629691331141?e=1653523200&v=beta&t=prseGQSfiYkHKsp7USCuHvQMtTiMu4NWvZ1-6i-7PsA",
+        LinkedIn: "https://www.linkedin.com/in/sarthak-tiwari-0398071b9/",
         GitHub: "https://github.com/sarthackk",
     },
     {
         name: "Keshav Garg",
         designation: "Project Development Lead",
-        imageURL: "https://media-exp1.licdn.com/dms/image/C4E03AQE7i_K_L-8bxg/profile-displayphoto-shrink_800_800/0/1572162297086?e=2147483647&v=beta&t=dO-QJoxM8yzY4-ltH-vOA2M5_R7llxs9u_gF4wpM3OQ",
-        LinkedIn: "https://www.linkedin.com/in/keshav-garg-883900163/",
+        imageURL: "https://media-exp1.licdn.com/dms/image/C4E03AQGX1uq4o6czEA/profile-displayphoto-shrink_800_800/0/1592208832830?e=1653523200&v=beta&t=lx8XDrUBhuxHWHeQtINnMbP6Hk8Oed31e6xozGwyPmo",
+        LinkedIn: "https://www.linkedin.com/in/keshav-garg-7669a9199/",
         GitHub: "https://github.com/keshav2410garg",
     },
     {
