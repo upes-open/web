5771784272cd88dbd798b5c06c807c1e55c5c439
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-04-09 12:51:22 +0530

    backend check

--- backend/FormServer.js
+++ backend/FormServer.js
@@ -32,6 +32,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/.netlify/functions/hello", (req, res) => {
+  res.json({ message: "Hello from Netlify Functions!" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
