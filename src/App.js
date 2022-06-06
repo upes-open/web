6c97f5241107ef766dd15c377d10cff2da4c8422
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-06-06 13:06:33 +0530

    osc pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ContactPage from "./screens/ContactPage";
 import EventPage from "./screens/EventPage";
 
 import AuthenticationPage from "./screens/AuthenticationPage";
-import CreateEvents from "./screens/CreateEvent";
+import OSCPage from "./screens/OSCPage";
 import Page404 from "./screens/Page404";
 import RegisterPage from "./screens/RegisterPage";
 
@@ -21,16 +21,9 @@ function App() {
         <Route path="/about" component={AboutPage} />
         <Route path="/contact" component={ContactPage} />
         <Route path="/events" component={EventPage} />
-
-        <Route path="/" exact component={HomePage} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/team" component={TeamPage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/events" component={EventPage} />
         <Route path="/authenticate" component={AuthenticationPage} />
-        <Route path="/CreateEvents" component={CreateEvents} />
-        <Route path="/registerpage" component={RegisterPage} />
+        <Route path="/osc" exact component={OSCPage} />
+        <Route path="/osc/register" component={RegisterPage} />
         <Route path="*" component={Page404} />
       </Switch>
     </Router>
