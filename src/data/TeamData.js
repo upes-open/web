08fb1707ced36abc44112c5b549704e5340f9e4f
Bhavika Jetwani <89550242+bhavika2502@users.noreply.github.com>
2022-06-24 21:31:20 +0530

    Update Teamdata.js

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -1,43 +1,43 @@
 export const TeamData = [
   {
-    name: "Khushboo Jakhmola",
+    name: "Raghav Agarwal",
     designation: "President",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C5603AQHK5AOKMiVs3A/profile-displayphoto-shrink_800_800/0/1564620462111?e=2147483647&v=beta&t=Tixe1LsTxK2XtHJYe90DsVsPsHV4YEX8n4GX_OefAv4",
-    LinkedIn: "https://www.linkedin.com/in/khushboo-jakhmola-a94406190/",
-    GitHub: "https://github.com/khushboo-jakhmola",
+      "https://www.linkpicture.com/q/raghav.jpg",
+    GitHub: "https://github.com/ragharwal",
+    LinkedIn: "https://www.linkedin.com/in/ragharwal/",
   },
   {
-    name: "Sanyam Mehendiratta",
+    name: "Priyal Gupta",
     designation: "Vice-President",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C5103AQEa9OCXf6Jidw/profile-displayphoto-shrink_800_800/0/1586493476926?e=1653523200&v=beta&t=hl_Efct7Woun9BKm0QjyiWaz2Fa5u4i8svYGeEyNRe8",
-    LinkedIn: "https://www.linkedin.com/in/sanyam-mehendiratta/",
-    GitHub: "https://github.com/Sanyam2000-dot",
+      "https://www.linkpicture.com/q/priyql.jpg",
+    LinkedIn: "https://www.linkedin.com/in/priyal-gupta-b86a01193/",
+    GitHub: "https://github.com/Priyal804",
   },
   {
     name: "Aradhya Singh",
     designation: "Treasurer",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C5603AQE64DOjXVHXsg/profile-displayphoto-shrink_800_800/0/1626534870614?e=1653523200&v=beta&t=TFaENfQVHW2OZQootoOiXMFRLIWM1ynLvdvGphUYO70",
+      "https://www.linkpicture.com/q/aradhya.jpg",
     LinkedIn: "https://www.linkedin.com/in/aradhya-singh-7993751a8/",
     GitHub: "https://github.com/optimizer-aa",
   },
   {
-    name: "Priyal Gupta",
+    name: "Sarthak Tiwari",
     designation: "Engagement Manager",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQHw47VW9AL0CQ/profile-displayphoto-shrink_800_800/0/1624515614986?e=1653523200&v=beta&t=tdZqgNvw-3x5ZOC5qJzYiamW7eTaR_icoI7_3eMcmBg",
-    LinkedIn: "https://www.linkedin.com/in/priyal-gupta-b86a01193/",
-    GitHub: "https://github.com/Priyal804",
+      "https://www.linkpicture.com/q/sarthak.jpg",
+    LinkedIn: "https://www.linkedin.com/in/sarthak-tiwari-0398071b9/",
+    GitHub: "https://github.com/sarthackk",
   },
   {
-    name: "Raghav Agarwal",
+    name: "Vedant Saraf",
     designation: "Collaboration Manager",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C5603AQHenarv5Nj6yg/profile-displayphoto-shrink_800_800/0/1637312459069?e=1653523200&v=beta&t=Tv9GPqWbB1PO7PPUlFg6fFHc2ofLI6pbuOkNso5GEm8",
-    LinkedIn: "https://www.linkedin.com/in/ragharwal/",
-    GitHub: "https://github.com/ragharwal",
+      "https://www.linkpicture.com/q/vedant.jpg",
+    LinkedIn: "https://www.linkedin.com/in/vedant-saraf-8a71b31b4/",
+    GitHub: "https://github.com/vedantsaraf2002",
   },
   {
     name: "Pranit Gupta",
@@ -51,56 +51,56 @@ export const TeamData = [
     name: "Nandini Nanda",
     designation: "Editorial Lead",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQGiYmlGxPv96A/profile-displayphoto-shrink_800_800/0/1628683541476?e=2147483647&v=beta&t=Aunabw4ZMggynI8h3MD2igJk84gFr-8OjXd0YbS-RWI",
+      "https://www.linkpicture.com/q/nandini.jpg",
     LinkedIn: "https://www.linkedin.com/in/nandini-nanda-835761205/",
-    GitHub: "",
+    GitHub: "https://github.com/NandiniNanda",
   },
   {
-    name: "Jigyasu Kukreja",
+    name: "Snigdha Parashar",
     designation: "PR-Marketing Lead",
-    imageURL: "https://avatars.githubusercontent.com/u/78949684?v=4",
-    LinkedIn: "https://www.linkedin.com/in/jigyasu-kukreja-1a63721a5/",
-    GitHub: "https://github.com/jigyasuk12",
+    imageURL: "https://www.linkpicture.com/q/snigdha.jpg",
+    LinkedIn: "https://www.linkedin.com/in/snigdha-parashar-022aaa23b/",
+    GitHub: "https://github.com/snigdha510",
   },
   {
-    name: "Sejal Jaiswal",
+    name: "Somyansh Avasthi",
     designation: "Social Media Lead",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQG6gdfNKo6gPQ/profile-displayphoto-shrink_800_800/0/1623842463939?e=1653523200&v=beta&t=H761EAe51LqKdjmx_ikYM99tgwYYse7fL0ocgybm2sE",
-    LinkedIn: "https://www.linkedin.com/in/sejal-jaiswal-067885212/",
+      "https://www.linkpicture.com/q/somyansh.jpg",
+    LinkedIn: "https://www.linkedin.com/in/somyansh-avasthi/",
+    GitHub: "https://github.com/SomyanshAvasthi",
+  },
+  {
+    name: "Amrita",
+    designation: "Design Lead",
+    imageURL:
+      "",
+    LinkedIn: "",
     GitHub: "",
   },
   {
-    name: "Sarthak Tiwari",
-    designation: "Design Lead",
-    imageURL:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQGaQtq0Ej-U6A/profile-displayphoto-shrink_800_800/0/1629691331141?e=1653523200&v=beta&t=prseGQSfiYkHKsp7USCuHvQMtTiMu4NWvZ1-6i-7PsA",
-    LinkedIn: "https://www.linkedin.com/in/sarthak-tiwari-0398071b9/",
-    GitHub: "https://github.com/sarthackk",
-  },
-  {
-    name: "Aditya Punetha",
+    name: "Bhavika Jetwani",
     designation: "Website Lead",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C5603AQGkBq6MZRnelA/profile-displayphoto-shrink_400_400/0/1601452227513?e=1653523200&v=beta&t=ybw0u7luND9ee34EzFdkp7VFvci8Mi6OgXMXIaneggo",
-    LinkedIn: "https://www.linkedin.com/in/aditya-punetha/",
-    GitHub: "https://github.com/AdityaPunetha",
+      "https://www.linkpicture.com/q/bhavika.jpeg",
+    LinkedIn: "https://www.linkedin.com/in/bhavika-jetwani-a1b47a220/",
+    GitHub: "https://github.com/bhavika2502",
   },
   {
-    name: "Keshav Garg",
-    designation: "Project Development Lead",
+    name: "Medhavi Singh",
+    designation: "Project Lead",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQGX1uq4o6czEA/profile-displayphoto-shrink_800_800/0/1592208832830?e=1653523200&v=beta&t=lx8XDrUBhuxHWHeQtINnMbP6Hk8Oed31e6xozGwyPmo",
-    LinkedIn: "https://www.linkedin.com/in/keshav-garg-7669a9199/",
-    GitHub: "https://github.com/keshav2410garg",
+      "https://www.linkpicture.com/q/medhavi.jpg",
+    LinkedIn: "https://www.linkedin.com/in/medhavi-singh-8752091ba/",
+    GitHub: "https://github.com/Med16-11",
   },
   {
-    name: "Ishika Kesarwani",
+    name: "Ashutosh Kapoor",
     designation: "External Collaboration Lead",
     imageURL:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQHpEcNQSafLqw/profile-displayphoto-shrink_800_800/0/1626580670698?e=2147483647&v=beta&t=Ef_sj9sMm8rMqOHB2QHWLvypai9vBw0ft6tG5CJukJk",
-    LinkedIn: "https://www.linkedin.com/in/ishika-kesarwani-3b32811a6/",
-    GitHub: "https://github.com/ishikkkkaaaa",
+      "https://www.linkpicture.com/q/ashutosh.jpg",
+    LinkedIn: "https://www.linkedin.com/in/kapoor0905/",
+    GitHub: "https://github.com/Kapoor-0905",
   },
   {
     name: "Medhavi Singh",
