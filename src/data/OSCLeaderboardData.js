1167ffcbc2d59ef12aa50edfbaed1ecca0b96e74
Aditya Punetha <71438448+AdityaPunetha@users.noreply.github.com>
2022-07-21 13:08:16 +0530

    Update OSCLeaderboardData.js

--- src/data/OSCLeaderboardData.js
+++ src/data/OSCLeaderboardData.js
@@ -24,24 +24,4 @@ export const OSCLeaderboardData = [
     name: "Rohin",
     points: "74",
   },
-  {
-    id: 6,
-    name: "Sparsh",
-    points: "62",
-  },
-  {
-    id: 7,
-    name: "Bhavika",
-    points: "59",
-  },
-  {
-    id: 8,
-    name: "Smriti",
-    points: "47",
-  },
-  {
-    id: 9,
-    name: "Akansha",
-    points: "46",
-  },
 ];
