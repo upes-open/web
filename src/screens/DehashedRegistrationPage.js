186d8417b105238f36a034728d8d1bad73166634
Bhavika Jetwani <89550242+bhavika2502@users.noreply.github.com>
2022-11-02 22:49:32 +0530

    Update DehashedRegistrationPage.js

--- src/screens/DehashedRegistrationPage.js
+++ src/screens/DehashedRegistrationPage.js
@@ -1,14 +1,8 @@
-import NavBar from '../components/Navbar'
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
 
 function DehashedRegistrationPage() {
     return (
         <div>
-            <NavBar />
-            <Contact />
-            <br/>
-            <Footer/>
+           window.location.href="https://dehashed.in/";
         </div>
     );
 }
