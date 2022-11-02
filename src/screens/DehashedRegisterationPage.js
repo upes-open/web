710bcab610e30f91981a0f6d446d7f77c930b653
Bhavika Jetwani <89550242+bhavika2502@users.noreply.github.com>
2022-11-02 22:28:17 +0530

    Update DehashedRegisterationPage.js

--- src/screens/DehashedRegisterationPage.js
+++ src/screens/DehashedRegisterationPage.js
@@ -1,2 +1,12 @@
-window.location.href="https://dehashed.in/";
-window.open("https://dehashed.in/");
+import React from "react";
+
+const DehashedRegistrationPage = () => {
+  return (
+    <div>
+    {window.location.href="https://dehashed.in/"};
+    {window.open("https://dehashed.in/")};
+    </div>
+  );
+};
+
+export default DehasedRegistrationPage;
